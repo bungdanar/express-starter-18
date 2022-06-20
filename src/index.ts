4053bb575c979f1311cc59612f592e4f064f559f
Bung Danar <danar.g.putera@gmail.com>
2022-06-20 13:40:09 +0700

    chore: Add express

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,26 @@
+import { app } from './app'
 import { Environment } from './utils/environment'
 
 const start = async () => {
+  let LISTENING_PORT = 5000
+
   try {
     Environment.checkEnvVariables()
+
+    // Reassign port
+    LISTENING_PORT = parseInt(Environment.APP_ENV.PORT!)
 
     console.log(`App has been lifted at ${new Date()}`)
   } catch (error) {
     console.error(error)
     process.exit(1)
   }
+
+  app.listen(LISTENING_PORT, () => {
+    console.log(
+      `⚡️[server]: Server is running at https://localhost:${LISTENING_PORT}`
+    )
+  })
 }
 
 start()
